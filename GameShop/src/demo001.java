cdcb03a8fcf501f751f93d4dc7d86aa23cd6f5c3
wFree1 <2095352855@qq,com>
2024-10-28 20:13:46 +0800

    demo001

--- GameShop/src/demo001.java
+++ GameShop/src/demo001.java
@@ -1,5 +1,5 @@
 public class demo001 {
     public static void main(String[] args) {
-        System.out.println("Hello, Java! This is a test program. Hello, Java! This is a test program. Hello, Java! This is a test program. Hello, Java! This is a test program.");
+        System.out.println("Hello, Git! This is my first commit.");
     }
 }
