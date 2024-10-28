891256782d82bf6b38b65b234f0fd56adfb2b111
Qi021212 <3377803605@qq.com>
2024-10-28 19:23:02 +0800

    DBUtil更新

--- GameShop/src/utils/DBUtil.java
+++ GameShop/src/utils/DBUtil.java
@@ -1,4 +1,60 @@
 package utils;
 
+import java.sql.*;
+
+//存放硬编码
 public class DBUtil {
+    private static final String DRIVER = "com.mysql.jdbc.Driver";
+    private static final String URL = "jdbc:mysql://localhost:3306/gameshop?useSSL=false&serverTimezone=UTC";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "zhenghanyu";
+
+    public static Connection getConnection(){
+        Connection connection = null;
+        try {
+            Class.forName(DRIVER);
+            connection=DriverManager.getConnection(URL,USERNAME,PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+    public static void close(Connection connection){
+        if(connection!=null){
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    public static void closeStatement(Statement statement){
+        if(statement!=null){
+            try {
+                statement.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    public static void closePreparedStatement(PreparedStatement preparedStatement){
+        if(preparedStatement!=null){
+            try {
+                preparedStatement.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    public static void closeResultSet(ResultSet resultSet){
+        if(resultSet!=null){
+            try {
+                resultSet.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
