428dacf6702f0ae55f549021cdb37dd9f3dd1a38
wFree1 <2095352855@qq,com>
2024-10-28 20:12:09 +0800

    demo001

---
+++ GameShop/src/demo001.java
@@ -0,0 +1,5 @@
+public class demo001 {
+    public static void main(String[] args) {
+        System.out.println("Hello, Java! This is a test program. Hello, Java! This is a test program. Hello, Java! This is a test program. Hello, Java! This is a test program.");
+    }
+}
